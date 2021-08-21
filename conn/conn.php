7bca19c7f5d2196040490a4691f9f0182df3ddc8
edison <defaltmkx@gmail.com>
2021-08-22 01:27:59 +0800

    fix typo

--- conn/conn.php
+++ conn/conn.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','hire_');
+define('DB_NAME','hire_me');
 
 
 if(!$con = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME))
